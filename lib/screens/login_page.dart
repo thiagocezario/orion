1f516be975d00f392f93a2ae7db7849185e045ac
Thiago Cezario <thiagocezario2@gmail.com>
2019-08-18 20:42:44 -0300

    Add form fields to login page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -4,16 +4,30 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      child: Material(
-        color: Colors.blue,
-        child: Center(
-          child: Text(
-            'Orion',
-            style: TextStyle(color: Colors.white, fontSize: 50.0),
+        alignment: Alignment.center,
+        child: Material(
+          color: Colors.blue,
+          child: Center(
+            child: LoginForm()
           ),
-        ),
-      )
+        ));
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+              'Orion',
+              style: TextStyle(color: Colors.white, fontSize: 50.0),
+            ),
+        TextFormField(),
+        TextFormField(),
+        ButtonBar()
+      ],
     );
   }
 }
