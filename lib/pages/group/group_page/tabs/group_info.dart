95f5494bb25e0eda2638d743b57e7c57a85fe1f5
Thiago Cezario <thiagocezario2@gmail.com>
2019-10-06 02:26:35 -0300

    Add leave group action

--- lib/pages/group/group_page/tabs/group_info.dart
+++ lib/pages/group/group_page/tabs/group_info.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:orion/api/client.dart';
 import 'package:orion/model/subscriptions.dart';
 import 'package:orion/model/user.dart';
+import 'package:orion/provider/my_groups_provider.dart';
+import 'package:provider/provider.dart';
 
 class GroupInfo extends StatelessWidget {
   final groupId;
@@ -9,12 +11,13 @@ class GroupInfo extends StatelessWidget {
   GroupInfo(this.groupId);
 
   void _exitGroup(BuildContext context) async {
-    await Client.listSubscriptions(
-            Singleton().jwtToken, groupId, Singleton().user.id.toString())
+    await Client.listSubscriptions(Singleton().jwtToken, groupId.toString(),
+            Singleton().user.id.toString())
         .then((response) async {
       var result = subscriptionsFromJson(response.body);
       await Client.unsubscribe(Singleton().jwtToken, result.first.id)
           .then((response) {
+        Provider.of<MyGroupsProvider>(context).refreshMyGroups();
         Navigator.of(context).pop();
       });
     });
