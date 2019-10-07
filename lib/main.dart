8012c235ddebf2edce93c172fe8da77254dc5890
Thiago Cezario <thiagocezario2@gmail.com>
2019-10-07 00:44:42 -0300

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:orion/provider/my_events_provider.dart';
 import 'package:orion/provider/my_groups_provider.dart';
 import 'package:orion/provider/subscriptions_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'model/user.dart';
 
 void main() => runApp(Orion());
@@ -18,14 +19,14 @@ class Orion extends StatelessWidget {
     Singleton();
 
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(builder: (context) => AuthProvider()),
-        ChangeNotifierProvider(builder: (context) => MyGroupsProvider()),
-        // ChangeNotifierProvider(builder: (context) => SearchGroupsProvider()),
-        ChangeNotifierProvider(builder: (context) => MyEventsProvider()),
-        ChangeNotifierProvider(builder: (context) => GroupEventsProvider()),
-        ChangeNotifierProvider(builder: (context) => SubscriptionsProvider()),
-      ],
+        providers: [
+          ChangeNotifierProvider(builder: (context) => AuthProvider()),
+          ChangeNotifierProvider(builder: (context) => MyGroupsProvider()),
+          // ChangeNotifierProvider(builder: (context) => SearchGroupsProvider()),
+          ChangeNotifierProvider(builder: (context) => MyEventsProvider()),
+          ChangeNotifierProvider(builder: (context) => GroupEventsProvider()),
+          ChangeNotifierProvider(builder: (context) => SubscriptionsProvider()),
+        ],
         child: MaterialApp(
           initialRoute: '/',
           routes: {
@@ -37,7 +38,29 @@ class Orion extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: LoginPage(),
+          home: SplashScreen(
+            seconds: 20,
+            backgroundColor: Colors.white,
+            imageBackground: AssetImage('assets/background/orion.jpg'),
+            image: Image.asset(
+              'assets/logo/orionlogo.png',
+              fit: BoxFit.fill,
+              height: 150,
+              width: 150,
+              scale: 100.0,
+
+            ),
+            loaderColor: Colors.white,
+            loadingText: Text(
+              'Carregando...',
+              style: TextStyle(color: Colors.white),
+            ),
+            title: Text(
+              'Bem vindo!',
+              style: TextStyle(color: Colors.white),
+            ),
+            navigateAfterSeconds: LoginPage(),
+          ),
         ));
   }
 }
