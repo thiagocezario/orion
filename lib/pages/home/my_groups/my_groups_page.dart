dfa2eb1643f49b22244da12be6a10ef8f7449746
Thiago Cezario <thiagocezario2@gmail.com>
2019-10-11 22:05:27 -0300

    Change layout from recommendation groups

--- lib/pages/home/my_groups/my_groups_page.dart
+++ lib/pages/home/my_groups/my_groups_page.dart
@@ -26,74 +26,107 @@ class _MyGroupsState extends State<MyGroups> {
             : const EdgeInsets.only(
                 left: 10.0, right: 10.0, top: 3.0, bottom: 30.0);
 
-        return new Padding(
+        return Padding(
           padding: padding,
-          child: new InkWell(
+          child: InkWell(
             onTap: () {},
-            child: new Container(
-              decoration: new BoxDecoration(
-                borderRadius: new BorderRadius.circular(10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
                 color: Colors.white,
                 boxShadow: [
-                  new BoxShadow(
+                  BoxShadow(
                       color: Colors.black.withAlpha(70),
                       offset: const Offset(3.0, 2.0),
                       blurRadius: 3)
                 ],
               ),
               width: 200.0,
-              child: new Stack(
+              child: Stack(
                 children: <Widget>[
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.only(left: 30, right: 30),
-                    child: Material(
-                      borderRadius: BorderRadius.circular(30.0),
-                      color: Colors.blue,
-                      child: MaterialButton(
-                        minWidth: MediaQuery.of(context).size.width,
-                        padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-                        onPressed: () {
-                          var singleton = Singleton();
-                          Client.subscribe(
-                                  singleton.jwtToken, recommendations[index].id)
-                              .then((response) {
-                            if (response.statusCode == 201) {
-                              Provider.of<MyGroupsProvider>(context)
-                                  .refreshMyGroups();
-                              Provider.of<GroupRecomendationsProvider>(context)
-                                  .refreshMyRecomendations();
-                              Provider.of<MyEventsProvider>(context)
-                                  .fetchEvents();
-                            }
-                          });
-                        },
-                        child: Text('Ingressar',
-                            textAlign: TextAlign.center,
-                            style: textStyle.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold)),
-                      ),
-                    ),
-                  ),
-                  new Align(
-                    alignment: Alignment.bottomCenter,
-                    child: new Container(
-                        decoration: new BoxDecoration(
-                            borderRadius: new BorderRadius.only(
-                                bottomLeft: new Radius.circular(10.0),
-                                bottomRight: new Radius.circular(10.0))),
-                        height: 30.0,
-                        child: new Row(
+                  // Container(
+                  //   alignment: Alignment.center,
+                  //   margin: EdgeInsets.only(left: 30, right: 30),
+                  //   child: Material(
+                  //     borderRadius: BorderRadius.circular(30.0),
+                  //     color: Colors.blue,
+                  //     child: MaterialButton(
+                  //       minWidth: MediaQuery.of(context).size.width,
+                  //       padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+                  //       onPressed: () {
+                  //         var singleton = Singleton();
+                  //         Client.subscribe(
+                  //                 singleton.jwtToken, recommendations[index].id)
+                  //             .then((response) {
+                  //           if (response.statusCode == 201) {
+                  //             Provider.of<MyGroupsProvider>(context)
+                  //                 .refreshMyGroups();
+                  //             Provider.of<GroupRecomendationsProvider>(context)
+                  //                 .refreshMyRecomendations();
+                  //             Provider.of<MyEventsProvider>(context)
+                  //                 .fetchEvents();
+                  //           }
+                  //         });
+                  //       },
+                  //       child: Text('Ingressar',
+                  //           textAlign: TextAlign.center,
+                  //           style: textStyle.copyWith(
+                  //               color: Colors.white,
+                  //               fontWeight: FontWeight.bold)),
+                  //     ),
+                  //   ),
+                  // ),
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            new Text(
-                              '${recommendations[index].name}',
-                              style: new TextStyle(color: Colors.black),
+                            Expanded(
+                                child: Text(
+                                  '${recommendations[index].name}'.toUpperCase(),
+                                  style:
+                                      TextStyle(color: Colors.black, fontSize: 15),
+                                  textAlign: TextAlign.center,
+                                ),
                             )
                           ],
-                        )),
-                  )
+                        ),
+                      )),
+                  Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.place),
+                            Expanded(
+                              child: Text(
+                                '${recommendations[index].institution.name}',
+                                style: TextStyle(color: Colors.black),
+                                textAlign: TextAlign.center,
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.people),
+                            Text(
+                              '${recommendations[index].metadata.subscriptions}',
+                              style: TextStyle(color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ))
                 ],
               ),
             ),
@@ -113,31 +146,34 @@ class _MyGroupsState extends State<MyGroups> {
         builder: (context, recommendations, _) => Container(
           child: Stack(
             children: <Widget>[
-              Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 25,
-                  ),
-                  // new Align(
-                  //   alignment: Alignment.centerLeft,
-                  //   child: new Padding(
-                  //       padding: new EdgeInsets.only(left: 8.0),
-                  //       child: new Text(
-                  //         'Recomendaçoes',
-                  //         style: new TextStyle(color: Colors.black),
-                  //       )),
-                  // ),
-                  Container(
-                    height: 140,
-                    width: MediaQuery.of(context).size.width,
-                    child: headerList(
-                        context, recommendations.groupRecomendations),
-                  ),
-                  Expanded(
-                    child: GroupCards()
-                        .getGroupCards(context, myGroupsState.myGroups),
-                  ),
-                ],
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                          padding: EdgeInsets.only(left: 8.0, bottom: 8),
+                          child: Text(
+                            'Recomendações',
+                            style: TextStyle(color: Colors.black),
+                          )),
+                    ),
+                    Container(
+                      height: 140,
+                      width: MediaQuery.of(context).size.width,
+                      child: headerList(
+                          context, recommendations.groupRecomendations),
+                    ),
+                    Expanded(
+                      child: GroupCards()
+                          .getGroupCards(context, myGroupsState.myGroups),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
