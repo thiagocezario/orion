c179ead9dafca03321de98254711d12c1107f157
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-11-04 20:46:10 -0300

    Add spesh screan and icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:orion/components/commom_items/commom_items.dart';
-import 'package:orion/pages/home/group/group_page/group_page.dart';
-import 'package:orion/pages/home/home_page.dart';
+import 'package:orion/pages/group/group_page.dart';
+import 'package:orion/pages/home_page.dart';
 import 'package:orion/pages/login/login_page.dart';
 import 'package:orion/pages/login/new_account_page.dart';
 import 'package:orion/pages/login/recover_password_page.dart';
@@ -14,7 +13,6 @@ import 'package:orion/provider/my_events_provider.dart';
 import 'package:orion/provider/my_groups_provider.dart';
 import 'package:orion/provider/subscriptions_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:splashscreen/splashscreen.dart';
 import 'model/user.dart';
 
 const String GroupPageRoute = '/group_page';
@@ -47,16 +45,7 @@ class Orion extends StatelessWidget {
           onGenerateRoute: generateRoute,
           debugShowCheckedModeBanner: false,
           title: 'Orion',
-          home: SplashScreen(
-            seconds: 5,
-            backgroundColor: primaryColor,
-            photoSize: 150,
-            loaderColor: Colors.white,
-            image: Image.asset(
-              'assets/logo/orionlogo.png',
-            ),
-            navigateAfterSeconds: LoginPage(),
-          ),
+          home: LoginPage(),
         ));
   }
 }
