dd51c844aee28733a7c888d93104f0fed1174623
Thiago Cezario <thiagocezario2@gmail.com>
2019-11-26 01:15:28 -0300

    Change Search result list FAB button color

--- lib/components/groups/filter_fab.dart
+++ lib/components/groups/filter_fab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:orion/model/course.dart';
 import 'package:orion/model/discipline.dart';
+import 'package:orion/model/global.dart';
 import 'package:orion/model/institution.dart';
 import 'package:orion/pages/group/new_group_page.dart';
 
@@ -16,7 +17,7 @@ class FilterFloatingButton extends StatelessWidget {
     return FloatingActionButton(
       child: Icon(Icons.add),
       elevation: 15.0,
-      backgroundColor: Colors.green,
+      backgroundColor: themeColor,
       onPressed: () {
         {
           Navigator.push(
