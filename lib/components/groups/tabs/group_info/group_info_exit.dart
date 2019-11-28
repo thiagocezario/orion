67f3aa3d91ce7b36f11fb2222b5a11a8680d717d
Thiago Cezario <thiagocezario2@gmail.com>
2019-11-28 15:04:25 -0300

    Add confirmation dialog for when leaving a group

--- lib/components/groups/tabs/group_info/group_info_exit.dart
+++ lib/components/groups/tabs/group_info/group_info_exit.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:orion/controllers/group_controller.dart';
 import 'package:orion/controllers/subscription_controller.dart';
 import 'package:orion/model/group.dart';
+import 'package:orion/provider/group_recomendations_provider.dart';
+import 'package:orion/provider/my_events_provider.dart';
+import 'package:orion/provider/my_groups_provider.dart';
+import 'package:provider/provider.dart';
 
 class GroupInfoExit extends StatelessWidget {
   final Group group;
@@ -8,9 +13,38 @@ class GroupInfoExit extends StatelessWidget {
   GroupInfoExit(this.group);
 
   void _exitGroup(BuildContext context) async {
-    SubscriptionController.removeFromGroup(context, group: group);
-    Navigator.of(context).pop();
-    Navigator.of(context).pop();
+    bool isExiting = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text("Você tem certeza que deseja sair do grupo?"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("CANCELAR"),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            FlatButton(
+              child: Text("SAIR"),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (isExiting) {
+      await SubscriptionController.removeFromGroup(context, group: group);
+      Provider.of<MyGroupsProvider>(context).refreshMyGroups();
+      Provider.of<GroupRecomendationsProvider>(context)
+          .refreshMyRecomendations();
+      Provider.of<MyEventsProvider>(context).fetchEvents();
+      Navigator.of(context).pop();
+      Navigator.of(context).pop();
+    }
   }
 
   @override
